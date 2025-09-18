379b5e998c40b4b5e394cb7437e29af7ffce1153
Adriel Teles <adrielt00@yahoo.com.br>
2025-09-18 11:52:48 -0300

    finished ex8_4

--- cap08/js/ex8_4.js
+++ cap08/js/ex8_4.js
@@ -3,54 +3,53 @@ const resp = document.querySelector("pre")
 
 const itens = []
 
+//evento de click radio pizza
 frm.rbPizza.addEventListener("click", () => {
     frm.inPizza.className = "exibe"
     frm.inBebida.className = "oculta"
-    
 })
 
+//evento de click radio bebiba
 frm.rbBebida.addEventListener("click", () => {
-    frm.inPizza.className = "oculta"
     frm.inBebida.className = "exibe"
-    
+    frm.inPizza.className = "oculta"
 })
 
-//quando o campo recebe o foco
+//evento que trás opções de sabores no campo input
 frm.inDetalhes.addEventListener("focus", () => {
-    if (frm.rbPizza.checked) {
+    if(rbPizza.checked){
         const pizza = frm.inPizza.value
         const num = pizza == "media" ? 2 : pizza == "grande" ? 3 : 4
-
-        frm.inDetalhes.placeholder = `Até ${num} sabores.`
-    }    
+        frm.inDetalhes.placeholder = `Até ${num} sabores`
+    }
 })
 
-//quando o campo perde o foco
+//quando tira o foco do input
 frm.inDetalhes.addEventListener("blur", () => {
     frm.inDetalhes.placeholder = ""
 })
 
-//evento de submit
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     let produtos
-    if(frm.rbPizza.checked) {
+    if (frm.rbPizza.checked) {
         const num = frm.inPizza.selectedIndex
-        produtos = frm.inPizza.options[num].text
+        produtos = frm.inPizza.options[num].text  
+        
     } else {
         const num = frm.inBebida.selectedIndex
-        produtos = frm.inBebida.options[num].text
+        produtos = frm.inBebida.options[num].text 
     }
 
     const detalhes = frm.inDetalhes.value
     itens.push(produtos + "(" + detalhes + ")")
 
     resp.textContent = itens.join("\n")
+
     frm.reset()
 
     frm.rbPizza.dispatchEvent(new Event("click"))
+
 })
-
-
 
