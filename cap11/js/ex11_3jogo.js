a59b100de5cd157c3a1611b6d72776c6ef79db70
Adriel Teles <adrielt00@yahoo.com.br>
2025-10-06 13:51:28 -0300

    finished cap11

--- cap11/js/ex11_3jogo.js
+++ cap11/js/ex11_3jogo.js
@@ -1,7 +1,7 @@
 const frm = document.querySelector("form")
 const respPalavra = document.querySelector("#outPalavra")
 const respErros = document.querySelector("#outErros")
-const respDica = document.querySelector("#outDicas")
+const respDica = document.querySelector("#outDica")
 const respChances = document.querySelector("#outChances")
 const respMensagemFinal = document.querySelector("#outMensagemFinal")
 const imgStatus = document.querySelector("img")
@@ -65,3 +65,73 @@ frm.btVerDica.addEventListener("click", () => {
 
     frm.inLetra.focus() // joga o foco em inLetra
 })
+
+const trocarStatus = (num) => {
+    if (num > 0) imgStatus.src = `./img/status${num}.jpg`
+}
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const letra = frm.inLetra.value.toUpperCase() // obtém o conteúdo do campo inletra
+
+    let erros = respErros.textContent
+    let palavra = respPalavra.textContent
+
+    //verifica se a letra apostada já consta em erros ou na palavra
+    if (erros.includes(letra) || palavra.includes(letra)) {
+        alert("Você já apostou essa letra")
+        frm.inLetra.focus()
+        return
+    }
+
+    //se letra const em palavra Sorteada
+    if(palavraSorteada.includes(letra)) {
+        let novaPalavra = "" //Para compor novaPalavra
+
+        //for para montar a palavra a ser exibida
+        for (let i = 0; i < palavraSorteada.length; i++) {
+            // se igual a letra apostada, acrescenta esta letra na exibição
+            if (palavraSorteada.charAt(i) === letra) {
+                novaPalavra += letra
+            } else {
+                novaPalavra += palavra.charAt(i) //se não, acrescenta letra ou "_" existente
+            }
+        }
+        respPalavra.textContent = novaPalavra
+    } else {
+        respErros.textContent += letra //acrescenta letras aos erros
+        const chances = Number(respChances.textContent) - 1 //diminui o número de chances
+        respChances.textContent = chances //exibe o novo número de chances
+
+        trocarStatus(chances) //troca imagem
+    }
+
+    verificarFim() //verifica se já ganhou ou perdeu
+
+    frm.inLetra.value = ""
+    frm.inLetra.focus()
+})
+
+const verificarFim = () => {
+    const chances = Number(respChances.textContent) //obtém o número de chances
+
+    if (chances == 0) {
+        respMensagemFinal.className = "display-3 text-danger"
+        respMensagemFinal.textContent = `Ah... é ${palavraSorteada}. Você perdeu!`
+        concluirJogo()
+    } else if (respPalavra.textContent == palavraSorteada) {
+        respMensagemFinal.className = "display-3 text-primary"
+        respMensagemFinal.textContent = "Parabéns!! Você ganhou."
+        trocarStatus(4) //exibe a figura do rostinho feliz
+        concluirJogo()
+    }
+}
+
+const concluirJogo = () => {
+    //modifica o texto da dica e desabilita os botões de jogar
+    respDica.textContent = "* Clique no Botão 'Iniciar jogo' para jogar novamente"
+    frm.inLetra.disabled = true
+    frm.btJogar.disabled = true
+    frm.btVerDica.disabled = true
+}
